62cd74a4ce1b99d0b553e19f27dbb0dec3880218
nftmoondogs <nftmoondogs@gmail.com>
2024-07-03 05:36:03 +0530

    newcommit

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
             <p>Jeets, don’t try me ; my gun don’t miss</p>
             <p>my gun got more body counts than these bitches </p>
             <div className='buttons'>
-              <a href='https://pump.fun/A4nra9BYDZCMPqtWUU81H8MjR8TDUJzsJMeeeJKVpump'>Buy on Pump.Fun</a>
+              <a href='https://pump.fun/board'>Buy on Pump.Fun</a>
             </div>
           </div>
         </div>
